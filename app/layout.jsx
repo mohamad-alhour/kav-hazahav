f835c98d0e2952f2609ab9d0b691d1a2990b2729
mohamad-alhour <mohamad.i.alhour@gmail.com>
2026-04-28 17:47:03 +0300

    Add files via upload

---
+++ app/layout.jsx
@@ -0,0 +1,7 @@
+export const metadata = {
+  title: 'קו הווילונות הזהב | מערכת ניהול',
+  description: 'מערכת ניהול יוקרתית ללקוחות, עובדים, משימות, מדידות והתקנות'
+};
+export default function RootLayout({ children }) {
+  return <html lang="he" dir="rtl"><body>{children}</body></html>;
+}
